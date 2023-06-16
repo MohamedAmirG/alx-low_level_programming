1c5cc443c3695209fc7f6b823461fe878a97924b
Mohamed <midodido977@gmail.com>
2023-06-15 19:02:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 prinf(s"The size of char is %ld bytes.\n",sizeof(char));
 prinf("The size of int is %ld bytes.\n",sizeof(int));
-prinf("The size of long int is %ld bytes.\n",sizeof(long int));
-prinf("The size of long long int is %ld bytes.\n",sizeof(long long int));
+prinf("The size of long int is %ld bytes.\n",sizeof(long));
+prinf("The size of long long int is %ld bytes.\n",sizeof(long long));
 prinf("The size of float is %ld bytes.\n",sizeof(float));
 return (0);
 }
