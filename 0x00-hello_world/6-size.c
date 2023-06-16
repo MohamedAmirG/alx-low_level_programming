c4fb8ac43e393f0fcdb06814f744c5c7d59b3eb6
Mohamed <midodido977@gmail.com>
2023-06-15 18:48:47 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+prinf(sizeof(char));
+prinf(sizeof(int));
+prinf(sizeof(long int));
+prinf(sizeof(long long int));
+prinf(sizeof(float));
+return (0);
+}
