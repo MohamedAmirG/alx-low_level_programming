04df0ab30aebc1378427c79e65f7e774ff6a2fad
Mohamed <midodido977@gmail.com>
2023-07-17 17:32:59 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,6 @@ if (!d)
 return;
 
 printf("Name: %s\n", d->name ? d->name : "(nil)");
-printf("Age: %f\n", d->age);	
+printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
